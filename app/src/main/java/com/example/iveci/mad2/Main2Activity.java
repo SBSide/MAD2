3ce251c0f2ba74ed488a6bcb7e79c407bda5e4a0
SBSide <ivecis@live.co.kr>
2017-03-23 16:41:08 +0900

    hw 3-1 complete. 170323.

--- app/src/main/java/com/example/iveci/mad2/Main2Activity.java
+++ app/src/main/java/com/example/iveci/mad2/Main2Activity.java
@@ -17,28 +17,26 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         tabhost = (TabHost) findViewById(R.id.thost1);
         tabhost.setup();
-        TabHost.TabSpec tab1 = tabhost.newTabSpec("달력").setContent(R.id.tab1)
-                .setIndicator("날짜선택");
-        tabhost.addTab(tab1);
-        TabHost.TabSpec tab2 = tabhost.newTabSpec("시계").setContent(R.id.tab2)
-                .setIndicator("선택");
-        tabhost.addTab(tab2);
+        tabhost.addTab(tabhost.newTabSpec("BMI").setContent(R.id.tab1)
+                .setIndicator("BMI계산기"));
+        tabhost.addTab(tabhost.newTabSpec("면적").setContent(R.id.tab2)
+                .setIndicator("면적 계산기"));
         tabhost.addTab(tabhost.newTabSpec("달력").setContent(R.id.tab3)
                 .setIndicator("정보입력"));
-        tabhost.addTab(tabhost.newTabSpec("달력").setContent(new TabHost.TabContentFactory() {
-            @Override
-            public View createTabContent(String tag) {
-                View view = View.inflate(Main2Activity.this,R.layout.layout2,null);
-
-                Button btn = (Button)view.findViewById(R.id.bgo);
-                btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(getApplicationContext(),"GO",Toast.LENGTH_SHORT).show();
-                    }
-                });
-                return view;
-            }
-        }).setIndicator("결과"));
+//        tabhost.addTab(tabhost.newTabSpec("달력").setContent(new TabHost.TabContentFactory() {
+//            @Override
+//            public View createTabContent(String tag) {
+//                View view = View.inflate(Main2Activity.this,R.layout.layout2,null);
+//
+//                Button btn = (Button)view.findViewById(R.id.bgo);
+//                btn.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Toast.makeText(getApplicationContext(),"GO",Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//                return view;
+//            }
+//        }).setIndicator("결과"));
     }
 }
